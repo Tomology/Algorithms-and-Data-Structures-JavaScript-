ab0f03ac3a35290c18cea873bfd0ef8b6c7b8f1e
Thomas Sobor <sobor.thomas@yahoo.com>
2020-10-28 16:49:24 -0700

    fix quicksort

--- sortingAlgorithms/quickSort.js
+++ sortingAlgorithms/quickSort.js
@@ -15,12 +15,12 @@ function quickSort(arr, left = 0, right = arr.length - 1) {
   return arr;
 }
 
-function pivot(arr, start = 0) {
+function pivot(arr, start = 0, end = arr.length - 1) {
   // We are assuming the pivot is always the first element
   let pivot = arr[start];
   let swapIdx = start;
 
-  for (let i = start + 1; i < arr.length; i++) {
+  for (let i = start + 1; i <= end; i++) {
     if (pivot > arr[i]) {
       swapIdx++;
       [arr[i], arr[swapIdx]] = [arr[swapIdx], arr[i]];
