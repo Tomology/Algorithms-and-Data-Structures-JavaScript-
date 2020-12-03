a9e2644e5866ea075912ad2678f1064afbf9b5cf
Thomas Sobor <sobor.thomas@yahoo.com>
2020-12-02 19:20:47 -0800

    add breadth first search into adjacency list

--- DataStructures/graphs/adjacencyList.js
+++ DataStructures/graphs/adjacencyList.js
@@ -69,4 +69,26 @@ class Graph {
       return result;
     }
   }
+
+  breadthFirst(start) {
+    const queue = [start];
+    let result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
