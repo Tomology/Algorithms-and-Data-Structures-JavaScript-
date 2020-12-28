a29c7d22410bd2ad1408321c69f7d93df12ae6d9
Thomas Sobor <sobor.thomas@yahoo.com>
2020-12-27 20:07:38 -0800

    fix minor mistakes

--- DynamicProgramming/recursion/canSum.js
+++ DynamicProgramming/recursion/canSum.js
@@ -1,5 +1,5 @@
 /*
-Write ae function 'canSum(targetSum, numbers)' that takes in a targetSum 
+Write a function 'canSum(targetSum, numbers)' that takes in a targetSum 
 and an array of numbers as arguments.
 The function should return a boolean indicating whether or not 
 it is possible to generate the targetSum using numbers from the array.
