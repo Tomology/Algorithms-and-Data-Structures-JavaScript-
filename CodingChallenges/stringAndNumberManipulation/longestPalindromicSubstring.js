10a4f27fa4b3180b181809d6a3da10f9015aafd1
Thomas Sobor <sobor.thomas@yahoo.com>
2021-02-16 19:51:43 -0500

    add longest palindromic substring

---
+++ CodingChallenges/stringAndNumberManipulation/longestPalindromicSubstring.js
@@ -0,0 +1,9 @@
+/*  
+    Input: s = "babad"
+    Output: "bab"
+    Note: "aba" is also a valid answer.
+*/
+
+const longestPalindromeSubstring = (s) => {};
+
+console.log(longestPalindromeSubstring("babad"));
