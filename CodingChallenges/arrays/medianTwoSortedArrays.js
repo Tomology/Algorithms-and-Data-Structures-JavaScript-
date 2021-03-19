f7f6fa033de17ce70bd32cbafc6214a83bd5fe9f
Thomas Sobor <sobor.thomas@yahoo.com>
2021-03-19 10:27:50 -0400

    add median of two sorted arrays challenge

---
+++ CodingChallenges/arrays/medianTwoSortedArrays.js
@@ -0,0 +1,34 @@
+var findMedianSortedArrays = function (nums1, nums2) {
+  const mergedArr = [];
+
+  let i = 0;
+  let j = 0;
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] < nums2[j]) {
+      mergedArr.push(nums1[i]);
+      i++;
+    } else {
+      mergedArr.push(nums2[j]);
+      j++;
+    }
+  }
+
+  while (i < nums2.length) {
+    mergedArr.push(nums1[i]);
+    i++;
+  }
+
+  while (j < nums2.length) {
+    mergedArr.push(nums2[j]);
+    j++;
+  }
+
+  let mid = mergedArr.length / 2;
+  if (mid % 1 === 0) {
+    return (mergedArr[mid] + mergedArr[mid - 1]) / 2;
+  } else {
+    return mergedArr[Math.floor(mid)];
+  }
+};
+
+console.log(findMedianSortedArrays([1, 2], [3, 4]));
